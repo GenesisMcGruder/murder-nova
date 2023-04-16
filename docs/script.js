f5bdf0ad0c08c112a6ee106b7db103e76f41aba5
Genesis McGruder <genesismcgruder@gmail.com>
2023-04-16 15:17:36 -0400

    Java update#2

--- docs/script.js
+++ docs/script.js
@@ -2,8 +2,9 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 
-const input = document.getElementById('button');
-function clickAlert() {
-    alert('I was clicked!');
+function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click',function() {
+        alert('I was clicked!');
+    })
 }
-input.addEventListener('click', clickAlert); 
