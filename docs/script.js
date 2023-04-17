04f14dbe333f40b0ab76e8d0e18fbb7410b857ce
Genesis McGruder <genesismcgruder@gmail.com>
2023-04-17 19:26:07 -0400

    update code

--- docs/script.js
+++ docs/script.js
@@ -2,11 +2,13 @@ const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 
-function addingEventListener() {
-    const input = document.getElementById('button');
-    input.addEventListener('click',function() {
-        alert('I was clicked!');
-    })
-}
-
-addingEventListener()
+const input = document.getElementById('button');
+input.addEventListener('click',function() {
+    alert('I was clicked!');
+})
+// function addingEventListener() {
+//     const input = document.getElementById('button');
+//     input.addEventListener('click',function() {
+//         alert('I was clicked!');
+//     })
+// }
